a68669815e020c6e74998caad85f0f653af47f2e
mjnoach <a.siennkiewicz@gmail.com>
2024-06-05 23:50:42 +0200

    update config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-const production = process.env.NODE_ENV === 'production'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: production ? 'export' : undefined,
-  basePath: production ? '/portfolio' : '',
 }
 
 module.exports = nextConfig
