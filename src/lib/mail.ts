85c3d019f77930cb23c876d68b01f165dfe8f52b
mjnoach <a.siennkiewicz@gmail.com>
2024-07-06 13:43:31 +0200

    test mailersend smtp with nodemailer

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,4 +1,33 @@
 import { EmailParams, MailerSend, Recipient } from 'mailersend'
+import nodemailer from 'nodemailer'
+
+console.log('🚀 ~ process.env.MAILERSEND_USER:', process.env.MAILERSEND_USER)
+
+let transporter = nodemailer.createTransport({
+  host: 'smtp.mailersend.net',
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.MAILERSEND_USER,
+    pass: process.env.MAILERSEND_PASS,
+  },
+})
+
+export const mail = {
+  // send: async (from: string, message: string) =>
+  //   mailersend.email.send(getEmailParams(from, message)),
+
+  send: (from: string, message: string) =>
+    transporter.sendMail({
+      from,
+      to: 'a.siennkiewicz@gmail.com',
+      subject: 'Mail Test',
+      text: `
+        from: ${from}\n
+        message:\n
+        ${message}`,
+    }),
+}
 
 const mailersend = new MailerSend({
   apiKey: process.env.MAILERSEND_API_KEY as string,
@@ -19,8 +48,3 @@ const getEmailParams = (from: string, message: string) =>
       ${message}`
     )
     .setTemplateId('yzkq340xzrx4d796')
-
-export const mail = {
-  send: async (from: string, message: string) =>
-    mailersend.email.send(getEmailParams(from, message)),
-}
